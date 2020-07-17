6b16b600fb795d88196e8ad84a23555f768b5401
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-16 22:33:04 -0400

    update to use ssl domain

--- src/dataFetch.js
+++ src/dataFetch.js
@@ -1,11 +1,11 @@
 const getStations = () => {
-    return fetch(`http://metronorthdistanceapp.com/all-stations/`, {
+    return fetch('https://nossonapptest7-14-20.azurewebsites.net/all-stations/', {
         method: "GET"
     })
 }
 
 const getTrainsApproaching = (stationId) => {
-    return fetch(`http://metronorthdistanceapp.com/trains-approaching/${stationId}/`, {
+    return fetch(`https://nossonapptest7-14-20.azurewebsites.net/trains-approaching/${stationId}/`, {
         method: "GET"
     })
     
